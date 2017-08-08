d4a3019608c0a365f3588be608cb20c787131712
frieddumplings <jwhcsc@gmail.com>
2017-08-09 04:16:17 +1000

    Implement Basic Post functionality - Randomly adding data from ajax call. Default value for string (post body).

--- DataMartApp/DataMartApp/Controllers/CarController.cs
+++ DataMartApp/DataMartApp/Controllers/CarController.cs
@@ -26,18 +26,29 @@ namespace DataMartApp.Controllers
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public List<seedData> Post([FromBody]string value)
         {
+            int data = db.seedData.OrderBy(x => x.id).Max(x => x.id);
+            seedData car = new seedData();
+            car.id = data + 1;
+            car.name = value;
+            Random random = new Random();
+            car.counter = random.Next(0, 1000000);
+            db.seedData.Add(car);
+            db.SaveChanges();
+            return db.seedData.ToList();
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public int Put(int id, [FromBody]string value)
         {
+            return 0;
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+
         }
     }
 }
