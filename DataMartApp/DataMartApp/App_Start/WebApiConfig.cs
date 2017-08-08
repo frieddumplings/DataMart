ccd914ad9d370d7a5b39076f6d5402090f5da2b9
frieddumplings <jwhcsc@gmail.com>
2017-08-09 02:13:23 +1000

    Implementation of some basic webAPI calls (GET Car Controller) api/cars

---
+++ DataMartApp/DataMartApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace DataMartApp.App_Start
+{
+    public class WebApiConfig
+    {
+
+
+        public static void RegisterRoutes(HttpConfiguration config)
+        {
+
+            
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate:"api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+                );
+        }
+    }
+}
---
+++ DataMartApp/DataMartApp/Controllers/CarController.cs
@@ -0,0 +1,43 @@
+﻿using DataMartApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace DataMartApp.Controllers
+{
+    public class CarController : ApiController
+    {
+        developmentEntities db = new developmentEntities();
+        // GET api/<controller>
+        public List<seedData> Get()
+        {
+            return db.seedData.ToList();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
