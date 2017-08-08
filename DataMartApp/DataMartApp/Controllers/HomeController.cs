1f6d0b0f9c7a31b6d5865a1d5790914f41493a85
frieddumplings <jwhcsc@gmail.com>
2017-08-09 01:16:36 +1000

    Add in Model for database seeded from AWS server. Implement basic view returning data from database as list of items

--- DataMartApp/DataMartApp/Controllers/HomeController.cs
+++ DataMartApp/DataMartApp/Controllers/HomeController.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using DataMartApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace DataMartApp.Controllers
 {
     public class HomeController : Controller
     {
+        developmentEntities db = new developmentEntities();
+        private DbSet<seedData> seedDataList;
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            seedDataList =  db.seedData;
+            return View(seedDataList.ToList());
         }
     }
 }
